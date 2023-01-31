9f6bdd09bffef05d5f62462c94cb9ed6c630cba9
masYuhadi <yuhadimaulana2306@gmail.com>
2023-01-31 15:42:55 +0700

    membuat website shoes care

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// toggle
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar navbar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
